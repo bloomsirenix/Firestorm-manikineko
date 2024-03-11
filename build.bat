e9a6a18a1717072b5b0eb853ccd114e00f1d6128
Daisy <66263341+bloomsirenix@users.noreply.github.com>
2024-03-11 14:29:45 +0100

    added packaging and fmod to build script

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 echo "Configure"
-autobuild configure -A 64 -c ReleaseFS_open -- --chan ManikinekoOnline -DLL_TESTS:BOOL=FALSE
+autobuild configure -A 64 -c ReleaseFS_open -- --chan ManikinekoOnline -DLL_TESTS:BOOL=FALSE -- --fmodstudio --package
 
 echo "Build"
 autobuild build -A 64 -c ReleaseFS_open --no-configure
